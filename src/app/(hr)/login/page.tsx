aff11d80365e16fcf6ce3fa5ca0ccb09442cd507
ridoy <ridoy@sspgroup.com>
2025-12-12 11:00:02 -0500

    fix bug

--- src/app/(hr)/login/page.tsx
+++ src/app/(hr)/login/page.tsx
@@ -1,5 +1,6 @@
 import LoginClient from "./LoginClient";
 
-export default function LoginPage({ searchParams }: { searchParams?: { errorMsg?: string } }) {
-  return <LoginClient errorMsg={searchParams?.errorMsg} />;
+export default async function LoginPage({ searchParams }: { searchParams?: Promise<{ errorMsg?: string }> | { errorMsg?: string } }) {
+  const sp = await Promise.resolve(searchParams); // works whether it's a Promise or plain object
+  return <LoginClient errorMsg={sp?.errorMsg} />;
 }
